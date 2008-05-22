8d4c054f571c5e9e2384d9ec5f5d41d0c1a95388
Brian Ford <bford@engineyard.com>
2008-05-22 09:01:24 -0700

    Some initial layout, style, and content.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def ellipsis(str, size=100)
+    return str if str.size <= size
+    str[0,str.size-size] = "..."
+    str
+  end
 end
