5730f98cd5f770a57c7cc66b7691641a18af1f53
Brian Ford <bford@engineyard.com>
2008-05-23 12:54:13 -0700

    Randomized banner text.

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,6 +1,13 @@
 class ExampleController < ApplicationController
+  BANNERS = [
+    ["l33t hax0rs", "use browsable backtraces"],
+    ["nothing comes between me", "and my browsable backtraces"],
+    ["Over 98% of IT professionals", "prefer browsable backtraces"]
+  ]
+
   def rescue_action(e)
     @exception = e
+    @banner = BANNERS[rand(BANNERS.size)]
     render :action => "show"
   end
 
